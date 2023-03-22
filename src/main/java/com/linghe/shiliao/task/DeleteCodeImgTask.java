4779b12811c94c29db8d8e21a9e3c66d31680846
zhirui <1796602716@qq.com>
2023-03-22 11:05:46 +0800

    123

--- src/main/java/com/linghe/shiliao/task/DeleteCodeImgTask.java
+++ src/main/java/com/linghe/shiliao/task/DeleteCodeImgTask.java
@@ -21,10 +21,13 @@ public class DeleteCodeImgTask {
     @Autowired
     private RedisCache redisCache;
 
-    @Scheduled(cron = "0/2 * * * * ?") //每个2秒执行一次
+    @Scheduled(cron = "0 * /1 * * * ?") //每个分钟执行一次
     // @Scheduled(cron="0 0/5 * * * *")
     private void deleteCodeImg() {
         List<String> uuidNames = this.getFileNames(codeImgPath);
+        if (uuidNames == null && uuidNames.size() == 0) {
+
+        }
         for (String uuidName : uuidNames) {
             String[] split = uuidName.split(".jpg");
             String uuid = split[0];
