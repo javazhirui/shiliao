d539f0961493d5ac329794ea27e579590c757202
zhirui <1796602716@qq.com>
2023-04-10 13:45:45 +0800

    minio测试1.2.3.0版本,距离成功更近近了

--- src/main/java/com/linghe/shiliao/config/MinioConfig.java
+++ src/main/java/com/linghe/shiliao/config/MinioConfig.java
@@ -1,39 +1,45 @@
 package com.linghe.shiliao.config;
 
 import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 /**
  * Minio 配置信息
  */
-@Configuration
-@ConfigurationProperties(prefix = "minio")
+@Component
 public class MinioConfig {
     /**
      * 外网访问服务地址
      */
+    @Value("{minio.url}")
     private String url;
 
     /**
      * 用户名
      */
+    @Value("{minio.accessKey}")
     private String accessKey;
 
     /**
      * 密码
      */
+    @Value("{minio.secretKey}")
     private String secretKey;
 
     /**
      * 桶名称
      */
+    @Value("{minio.bucketName}")
     private String bucketName;
 
     /**
      * 外网访问服务地址
      */
+    @Value("{minio.previewUrl}")
     private String previewUrl;
 
     @Bean
